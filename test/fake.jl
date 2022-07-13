09039adc345c09dfeafdd160eced2e5e9722f5a4
alicedb2 <alice.doucet.beaupre@gmail.com>
2022-07-13 11:26:32 -0400

    minor adjustement

--- test/fake.jl
+++ test/fake.jl
@@ -1,22 +1,24 @@
 using Random: seed!
 using Distributions: MultivariateNormal
 using LinearAlgebra: diagind
-
+using StatsBase: mean
 using MultivariateNormalCRP
 
 function fake()
     
     # Seed the fake data
-    seed!(1234)
+    seed!(2345)
     # Generate some test data
     d = 2
     nb_clusters = 8
     samples_per_cluster = 50
     mu0 = fill(0.0, d)
-    lambda0 = 0.01
-    psi0 = -ones(d, d)
-    psi0[diagind(psi0)] .= 2.0 * d
-    nu0 = d - 1.0 + 10.0
+    lambda0 = 0.03
+    psi0 = -1.0 * rand(d, d)
+    psi0 = 0.5 * (psi0 + psi0')
+    psi0[diagind(psi0)] = 10.0 * rand(d)
+    nu0 = d - 1.0 + 17.0
+    display(psi0)
 
     presences = zeros(d, 0)
     fake_state = Vector{Set{Vector{Float64}}}()
@@ -28,6 +30,8 @@ function fake()
         presences = hcat(presences, samples)
         push!(fake_state, Set([1.0 * col for col in eachcol(samples)]))
     end
+    
+    display(plot_pi_state(fake_state, title="Fake data"))
 
     # Seed the MCMC
     seed!(41)
@@ -37,7 +41,7 @@ function fake()
     chain_state = initiate_chain(data)
 
     # Profile split-merge moves
-    @time @profview advance_chain!(chain_state, nb_steps=20, nb_gibbs=0, nb_splitmerge=5, nb_paramsmh=0, fullseq_prob=0.0)
+    @time @profview advance_chain!(chain_state, nb_steps=20, nb_gibbs=0, nb_splitmerge=5, nb_paramsmh=0, fullseq_prob=0.03)
 
     # Profile Gibbs moves
     # @time @profview advance_chain!(chain_state, nb_steps=20, nb_gibbs=5, nb_splitmerge=0, nb_paramsmh=0, fullseq_prob=0.0)
@@ -45,12 +49,13 @@ function fake()
     # Profile Metropolis-Hastings moves for parameters
     # @time @profview advance_chain!(chain_state, nb_steps=20, nb_gibbs=0, nb_splitmerge=0, nb_paramsmh=10, fullseq_prob=0.0)
 
+    # Profile all
+    # @time @profview advance_chain!(chain_state, nb_steps=20)
+
     return fake_state, chain_state
 end
 
-fake_state, chain_state = fake();
-plot_pi_state(fake_state, title="Fake data")
+fake_state, chain_state = fake(); nothing;
 
-# advance_chain!(chain_state)
-# plot_pi_state(chain_state.map_pi, title="MAP state")
-# plot_pi_state(chain_state.pi_state, title="Current chain state")
+# advance_chain!(chain_state, nb_steps=300)
+# plot_chain(chain_state, burn=100)
