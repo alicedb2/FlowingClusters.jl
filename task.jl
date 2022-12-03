be2b2fb5905e324a32765cd1cee313b1f8864693
alicedb2 <alice.doucet.beaupre@gmail.com>
2022-12-03 12:16:31 -0500

    expanduser, 30k steps

--- task.jl
+++ task.jl
@@ -10,12 +10,12 @@ chain = initiate_chain(dataset["chain_input"])
 
 advance_chain!(chain, nb_steps=2000, 
 nb_splitmerge=300, nb_gibbs=1, fullseq_prob=0.0,
-checkpoint_every=-1, checkpoint_prefix="tiger7k",
+checkpoint_every=-1, checkpoint_prefix=expanduser("~/scratch/tiger/tiger7k"),
 pretty_progress=false)
 
 burn!(chain, 2000)
 
-advance_chain!(chain, nb_steps=20000, 
+advance_chain!(chain, nb_steps=30000,
 nb_splitmerge=300, nb_gibbs=1, fullseq_prob=0.0,
-checkpoint_every=5000, checkpoint_prefix="tiger7k",
+checkpoint_every=5000, checkpoint_prefix=expanduser("~/scratch/tiger/tiger7k"),
 pretty_progress=false)
