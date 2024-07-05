6e8272dc5f0b1d27ea6f0e65a159a0a37bded058
Alice Doucet Beaupré <alice.doucet.beaupre@gmail.com>
2024-07-04 20:06:07 -0400

    removed commented code

--- src/helpers.jl
+++ src/helpers.jl
@@ -1,78 +1,25 @@
-using DataFrames: DataFrame, DataFrameRow
-# import GBIF: GBIFRecord, GBIFRecords, occurrences
-# using CSV
-
-# function GBIFRecord(row::DataFrameRow)
-
-#     row = Dict(names(row) .=> values(row))
-
-#     levels = ["kingdom", "phylum", "class", "order", "family", "genus", "species"]
-#     for level in levels
-#         push!(row, level * "Key" => pop!(row, level))
-#     end
-
-#     push!(row, "key" => pop!(row, "gbifID"))
-
-#     issues = pop!(row, "issue")
-#     push!(row, "issues" => ismissing(issues) ? String[] : split(issues, ";"))
-
-#     return GBIFRecord(row)
-
-# end
-
-# function GBIFRecords(dataframe::DataFrame)
-#     query = nothing
-#     records = GBIFRecord.(eachrow(dataframe))
-
-#     return GBIFRecords(query, records)
-# end
-
-# occurrences(dataframe::DataFrame) = GBIFRecords(dataframe)
-# occurrences(filename::AbstractString) = GBIFRecords(DataFrame(File(filename)))
-
-
-# function generate_pseudoabsences(records::GBIFRecords, layers::Vector{<:SimpleSDMLayer}; n=nothing, method=WithinRadius, distance=1.0)
-#     presences_only = mask(clip(first(layers), records), records, Bool)
-#     if method == WithinRadius
-#         psabs_lonlats = rand(method, presences_only, distance=distance)
-#     else
-#         psabs_lonlats = rand(method, presences_only)
-#     end
-#     psabs_lonlats = keys(psabs_lonlats)[collect(psabs_lonlats)]
-#     if n !== nothing
-#         n = min(n, length(psabs_lonlats))
-#         psabs_longlats = sample(psabs_lonlats, n, replace=false)
-#     end
-#     psabs_predictors = Vector{Float64}[Float64[layer[lonlat] for layer in layers] for lonlat in psabs_lonlats]
-#     return (predictors=psabs_predictors, longitudes=getindex.(psabs_lonlats, 1), latitudes=getindex.(psabs_lonlats, 2))
-# end
-
-# function generate_pseudoabsences(dataframe::DataFrame, layers::Vector{<:SimpleSDMLayer}; n=nothing, method=WithinRadius, distance=1.0)
-#     records = GBIFRecords(dataframe)
-#     return generate_pseudoabsences(records, layers, n=n, method=method, distance=distance)
-# end
-
-
 function performance_statistics(scores_at_presences, scores_at_absences; threshold=nothing)
+    
     if threshold !== nothing
-        # Scores of 1 always mean a presence,
-        # so an absence with a score of 1 means a false positive
         scores_at_presences = scores_at_presences .>= threshold
         scores_at_absences = scores_at_absences .>= threshold
     end
 
+    # presence with a score towards 1 is a true positive
     tp = sum(scores_at_presences)
-    fn = sum(1 .- scores_at_presences)
-
-    # absence with a score towards 0, therefore 1 - score -> 1
-    # is a true negative
+    # absence with score towards 1 is a false positive
+    fp = sum(scores_at_absences)
+    
+    # absence with a score towards 0 is a true negative
     tn = sum(1 .- scores_at_absences)
-    fp = sum(scores_at_absences) # absence with score towards 1 is a false positive
+    # presence with score towards 0 is a false negative
+    fn = sum(1 .- scores_at_presences)
+    
 
     sensitivity = tp/(tp + fn)
     specificity = tn/(tn + fp)
-    MCC = (tp * tn - fp * fn) / sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
     J = sensitivity + specificity - 1
+    MCC = (tp * tn - fp * fn) / sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
     kappa = 2 * (tp * tn - fn * fp)/((tp + fp) * (fp + tn) + (tp + fn) * (fn + tn))
     ppv = tp / (tp + fp)
     npv = tn / (tn + fn)
@@ -81,28 +28,13 @@ function performance_statistics(scores_at_presences, scores_at_absences; thresho
             MCC, J, kappa,
             sensitivity, specificity,
             ppv, npv,
-            tp, fn, tn, fp,
+            tp, tn, fp, fn
             )
 
 end
 
 
-function drawNIW(
-    mu::AbstractVector{Float64},
-    lambda::Float64,
-    psi::AbstractMatrix{Float64},
-    nu::Float64)::Tuple{Vector{Float64}, Matrix{Float64}}
-
-    invWish = InverseWishart(nu, psi)
-    sigma = rand(invWish)
-
-    multNorm = MvNormal(mu, sigma/lambda)
-    mu = rand(multNorm)
-
-    return mu, sigma
-end
-
-function best_score_threshold(scores_at_presences, scores_at_absences; statistic=:J, nbsteps=1000)
+function best_score_threshold(scores_at_presences, scores_at_absences; statistic=:MCC, nbsteps=1000)
     
     thresholds = LinRange(0.0, 1.0, nbsteps)
     best_score = -Inf
@@ -179,3 +111,18 @@ function doane(x::AbstractArray)
     return 1 + log2(n) + log2(1 + abs(skw) / sg1)
 end
 
+
+function drawNIW(
+    mu::AbstractVector{Float64},
+    lambda::Float64,
+    psi::AbstractMatrix{Float64},
+    nu::Float64)::Tuple{Vector{Float64}, Matrix{Float64}}
+
+    invWish = InverseWishart(nu, psi)
+    sigma = rand(invWish)
+
+    multNorm = MvNormal(mu, sigma/lambda)
+    mu = rand(multNorm)
+
+    return mu, sigma
+end
