15488c91099d1ae58ae17f898418987447724394
Alice Doucet Beaupré <alice.doucet.beaupre@gmail.com>
2024-07-05 02:35:22 -0400

    align with FCDataset

--- misc/bioclim_predictions.jl
+++ misc/bioclim_predictions.jl
@@ -1,10 +1,13 @@
-bioclim = bioclim_predictor(reduce(hcat, train_presences.data))
 
-bioclim_validation_presence_scores = bioclim(reduce(hcat, validation_presences.data))
-bioclim_validation_absence_scores = bioclim(reduce(hcat, validation_absences.data))
-bioclim_test_presence_scores = bioclim(reduce(hcat, test_presences.data))
-bioclim_test_absence_scores = bioclim(reduce(hcat, test_absences.data))
 
+bioclim = bioclim_predictor(eb.presence(:sp5).standardize(:BIO1, :BIO3, :BIO12))
+
+bioclim_validation_presence_scores = bioclim(eb.validation.presence(:sp5).standardize(:BIO1, :BIO3, :BIO12))
+bioclim_validation_absence_scores = bioclim(eb.validation.absence(:sp5).standardize(:BIO1, :BIO3, :BIO12))
+bioclim_test_presence_scores = bioclim(eb.test.presence(:sp5).standardize(:BIO1, :BIO3, :BIO12))
+bioclim_test_absence_scores = bioclim(eb.test.absence(:sp5).standardize(:BIO1, :BIO3, :BIO12))
+
+perfstat = :MCC
 best_bioclim_thresh = best_score_threshold(bioclim_validation_presence_scores, bioclim_validation_absence_scores, statistic=perfstat)
 
 bioclim_test_statistics = map(x -> round(x, digits=3), performance_statistics(bioclim_test_presence_scores, bioclim_test_absence_scores))
