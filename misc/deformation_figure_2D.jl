bf7f8b4f583cfbe31ee8a31af15e2f363a52064d
Alice Doucet Beaupré <alice.doucet.beaupre@gmail.com>
2024-07-11 14:50:52 -0400

    stashing some misc

--- misc/deformation_figure_2D.jl
+++ misc/deformation_figure_2D.jl
@@ -32,7 +32,7 @@ function deformation_figure_2d(chain)
 
 
     fig = Figure(size=(900, 500));
-    ax1 = Axis(fig[1, 1], xlabel="Real axis 1 -> Base axis 1", ylabel="Real axis 2 -> Base axis 2")
+    ax1 = CairoMakie.Axis(fig[1, 1], xlabel="Real axis 1 -> Base axis 1", ylabel="Real axis 2 -> Base axis 2")
     scatter!(ax1, basespace[1, :], basespace[2, :], markersize=2, label=nothing); 
     scatter!(ax1, realprobgrid[1, :], realprobgrid[2, :], color=:grey, alpha=0.5, markersize=2, label=nothing);
 
@@ -40,7 +40,7 @@ function deformation_figure_2d(chain)
     scatter!(ax1, Float64[], Float64[], color=Cycled(1), markersize=15, label="Base space"); 
     ylims!(ax1, nothing, 7); axislegend(ax1, framecolor=:white); 
 
-    ax2 = Axis(fig[1, 2], xlabel="Base axis 1 -> Real axis 1", ylabel="Base axis 2 -> Real axis 2")
+    ax2 = CairoMakie.Axis(fig[1, 2], xlabel="Base axis 1 -> Real axis 1", ylabel="Base axis 2 -> Real axis 2")
     scatter!(ax2, realspace[1, :], realspace[2, :], markersize=2, label=nothing); 
     scatter!(ax2, baseprobgrid[1, :], baseprobgrid[2, :], color=:grey, alpha=0.5, markersize=2, label=nothing);
     scatter!(ax2, Float64[], Float64[], color=:grey, markersize=15, label="Base space");
